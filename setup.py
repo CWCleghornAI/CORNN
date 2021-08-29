752426b1b56f76b14462599e8a313ec46465aa25
ChrisCleg <chriscleghorn89@gmail.com>
2021-08-29 21:41:55 +0200

    readme update with install instructions

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='CORNN',
+      version='0.9',
+      description='CORNN -- Continuous Optimisation of Regression tasks using Neural Networks',
+      author='CWCleghorn',
+      url='https://github.com/CWCleghornAI/CORNN',
+      packages=['CORNN']
+)
